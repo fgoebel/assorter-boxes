3046119d29b27d521af85cc6dfc057c9d44fc005
Fabian Goebel <fabian.goebel@fgdo.de>
2021-10-11 22:28:33 +0200

    add render script for schubladen boxes

---
+++ schublade_grid.scad
@@ -0,0 +1,11 @@
+// grids ;-D
+
+include <defaults_mini_schublade.scad>
+include <assorter.scad>
+
+x = 1;
+y = 1;
+
+gap = 2.4 ; // gap between the grid in x axis and the Schublade
+
+grid();
---
+++ schublade_render.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+output_path="stl-schublade"
+x_max=4
+y_max=4
+
+[ -d "$output_path" ] || mkdir -p $output_path
+
+echo "Render boxes"
+
+for ((x=1;x<=x_max;x++)); do
+    for ((y=1;y<=y_max;y++)); do
+        openscad -o $output_path/box-$x-$y.stl -D x=$x -D y=$y schublade_box.scad 1>/dev/null 2>&1 &
+    done
+done
+
+echo "render spaced grid"
+
+openscad -o $output_path/grid-2-4_spacer.stl schublade_grid-2x4-spacer.scad 1>/dev/null 2>&1 &
+
+echo "render grids"
+
+for ((x=1;x<=x_max;x++)); do
+    for ((y=1;y<=y_max;y++)); do
+        openscad -o $output_path/grid-$x-$y.stl -D x=$x -D y=$y schublade_grid.scad 1>/dev/null 2>&1 &
+    done
+done
